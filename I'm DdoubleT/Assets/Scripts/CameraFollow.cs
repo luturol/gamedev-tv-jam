7ab7956509e9c21f2876c0eb81a3460b63d0e43f
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-05-06 20:50:47 -0300

    Add CameraFollow script

---
+++ I'm DdoubleT/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform playerTransform;
+    public float offset;
+
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+    }
+
+    
+    void LateUpdate()
+    {    
+        Vector3 cameraTemp = transform.position;
+        cameraTemp.x = playerTransform.position.x;
+        
+        transform.position = cameraTemp;
+    }
+}
