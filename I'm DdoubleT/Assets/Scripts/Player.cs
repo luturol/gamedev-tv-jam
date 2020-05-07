ac3a26776b5992cb18560b2fefa0b6088b24fd1a
marcospadilha92 <marcospad92@gmail.com>
2020-05-06 22:19:24 -0300

    - script inicial movimento

---
+++ I'm DdoubleT/Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    // variável de virada do sprite
+    private bool facingRight = true;
+
+    // Variáveis correspondentes a gravidade quanto ao chão do personagem 
+    private bool onGround;
+    public Transform groundCheck;
+
+    //pulo
+    private bool jump = false;
+    private bool doubleJump;
+    public float jumpForce;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    // inverter o sprite baseado na scale 
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+
+        transform.localScale = scale;
+    }
+
+    void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+
+        transform.position += movement * Time.deltaTime * speed;
+
+        if (movement.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (movement.x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
